b4266ceb171ca86453e71327c5c6a890852d7cb3
GiliMinkovitz <gili0504126522@gmail.com>
2026-03-13 16:26:54 +0200

    setup: connection to DB

---
+++ Data/SchedulingDbContext.cs
@@ -0,0 +1,26 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace SchedulingService.Data;
+
+public class SchedulingDbContext : DbContext
+{
+
+    public SchedulingDbContext(){}
+    public SchedulingDbContext(DbContextOptions<SchedulingDbContext> options) : base(options){}
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (optionsBuilder.IsConfigured)
+            return;
+        // Called from the Environment Variable
+        var connectionString = Environment.GetEnvironmentVariable("SCHEDULING_DB_CS");
+        if (string.IsNullOrWhiteSpace(connectionString))
+        {
+            throw new InvalidOperationException(
+                "The SCHEDULING_DB_CS environment variable is not set.");
+        }
+        optionsBuilder.UseSqlServer(connectionString);
+    }
+    // DbSet-s will come here...
+}            
