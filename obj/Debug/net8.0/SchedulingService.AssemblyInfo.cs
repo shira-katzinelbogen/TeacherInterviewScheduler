f8be505eec4232302502091bd5443872ef2d0d68
GiliMinkovitz <gili0504126522@gmail.com>
2026-03-17 01:58:54 +0200

    refactor: remove redundant using statements

--- obj/Debug/net8.0/SchedulingService.AssemblyInfo.cs
+++ obj/Debug/net8.0/SchedulingService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SchedulingService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26d3988c8e7e5fc6f3237c2dd24f5444d83c25fd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+427c2bdd1556c3481a4a045d52e366f6b55a8dd5")]
 [assembly: System.Reflection.AssemblyProductAttribute("SchedulingService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SchedulingService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
